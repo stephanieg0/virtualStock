1452d069b54606276ef709941cbbfdc23214fab0
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-15 21:28:47 -0600

    added views file and jade layout and index files

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const route = express();
 
 const PORT = process.env.PORT || 3000;
 
+//setting jade files
+route.set('view engine', 'jade');
+
 route.listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
 });
