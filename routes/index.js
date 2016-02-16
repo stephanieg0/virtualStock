f2e79eaa35cf5e30185d77bb820e942451fd7296
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-15 22:14:54 -0600

    jade files and server are connected

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const home = require('./home');
+
+router.use(home);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict';
 //dependencies
 const express = require('express');
-const route = express();
+const app = express();
+const bodyParser = require('body-parser');
+const routes = require('./routes/');
 
 const PORT = process.env.PORT || 3000;
 
 //setting jade files
-route.set('view engine', 'jade');
+app.set('view engine', 'jade');
+//parsing data to send to dom
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//use routes
+app.use(routes);
 
-route.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
 });
